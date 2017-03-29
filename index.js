570a7cda1bf4094957ee998fb3297eccf124a56b
Mark Wallsgrove <mark.wallsgrove+git@gmail.com>
2017-03-29 23:55:34 +0100

    Fixed progress bars & journal

--- index.js
+++ index.js
@@ -1,83 +1,161 @@
 const term = require('terminal-kit').terminal;
 const Plugins = require('js-plugins');
+const Luxafor = require('luxafor')();
 const async = require('async');
+const usb = require('usb');
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+terminator = (signal) => {
+    if (typeof signal === 'string') {
+       process.exit(1);
+    }
+};
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+ 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+].forEach(function(element, index, array) {
+    process.on(element, () => { terminator(element); });
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
 
 // setup plugins
 const pluginManager = new Plugins();
 pluginManager.scan();
 
-// register progress bar plugin
-// TODO: make this more generic, rest & work are pretty much the same
-// just use before and after then send in the stage name within the event
-pluginManager.register('luxafor-linux-pomodoro:events', 'ProgressBar', () => ({
-    beforeWork: (conf) => {
-        Promise((fulfill) => {
-            this.progressBar = term.progressBar({
-                width: 80,
-                title: conf.title,
-                eta: true,
-                percent: true,
+pluginManager.register('luxafor-linux-pomodoro:events', 'ProgressBar', () => {
+    term.on('key', (name , matches , data) => {
+        if (name === 'CTRL_C') {
+            term.grabInput( false ) ;
+            setTimeout(() => { process.exit(); }, 100) ;
+        }
+    }) ;
+
+    function progressBar(conf, cb) {
+        this.progressBar = term.progressBar({
+            width: 80,
+            title: conf.title,
+            eta: true,
+            percent: true,
+        });
+
+        cb();
+    }
+
+    return {
+        work: (conf) => {
+            return new Promise((fulfill) => {
+                progressBar.bind(this)(conf, fulfill);
             });
-
-            fulfill();
-        });
-    },
-    beforeRest: (conf) => {
-        Promise((fulfill) => {
-            this.progressBar = term.progressBar({
-                width: 80,
-                title: conf.title,
-                eta: true,
-                percent: true,
+        },
+        rest: (conf) => {
+            return new Promise((fulfill) => {
+                progressBar.bind(this)(conf, fulfill);
             });
+        },
+        tick: (event) => {
+            return new Promise((fulfill) => {
+                this.progressBar.update(event.progress);
+                if (event.progress >= 1) {
+                    term('\n');
+                }
 
-            fulfill();
-        });
-    },
-    tick: (event) => {
-        Promise((fulfill) => {
-            this.progressBar.update(event.progress);
-            fulfill();
-        });
-    },
-    afterRest: () => {
-        Promise((fulfill) => {
-            term('\n');
-            fulfill();
-        });
-    },
-    afterWork: () => {
-        Promise((fulfill) => {
-            term('\n');
-            fulfill();
-        });
-    },
-}));
+                fulfill();
+            });
+        },
+    };
+});
 
+pluginManager.register('luxafor-linux-pomodoro:events', 'journal', () => {
+    term.on('key', (name , matches , data) => {
+        if (name === 'CTRL_C') {
+            term.grabInput( false ) ;
+            setTimeout(() => { process.exit(); }, 100) ;
+        }
+    }) ;
+
+    return {
+        beforerest: (conf) => {
+            // TODO record this information in a file
+            return new Promise((fulfill) => {
+                term('what awesomeness have you demonstrated: ') ;
+                term.inputField({ cancelable: true }, (err, input) => {
+                    term('\n');
+                    fulfill();
+                });
+            });
+        },
+    };
+});
+
+// most of this is just configuration rather than code
+// remove the precise task such as 'beforework' and
+// only implement 'before', 'on', 'tick', 'after' then
+// use the configuration object for colours, etc
+pluginManager.register('luxafor-linux-pomodoro:events', 'luxafor-flag', () => {
+    function luxaforMissing() {
+        // https://github.com/dave-irvine/node-luxafor/issues/1
+        // hide bug in library where it doesn't check for the
+        // existence of the usb device
+        return usb.findByIds(Luxafor.vid, Luxafor.pid) == null;
+    }
+
+    return {
+        beforework: (conf) => {
+            return new Promise((fulfill) => {
+                if (luxaforMissing()) {
+                    fulfill();
+                    return;
+                }
+
+                Luxafor.init(() => {
+                    Luxafor.setLuxaforColor(Luxafor.colors.red, fulfill);
+                });
+            });
+        },
+        beforerest: (conf) => {
+            return new Promise((fulfill) => {
+                if (luxaforMissing()) {
+                    fulfill();
+                    return;
+                }
+
+                Luxafor.init(() => {
+                    Luxafor.setLuxaforColor(Luxafor.colors.green, fulfill);
+                });
+            });
+        },
+    };
+});
 
 function sendEvent(state, method, cb) {
     pluginManager.connect({}, 'luxafor-linux-pomodoro:events', { multi: true }, (err, plugins) => {
         async.each(plugins, (plugin, pluginCb) => {
-            plugin[method](state).then(pluginCb);
+            if (plugin[method]) {
+                plugin[method](state).then(pluginCb);
+            } else {
+                pluginCb();
+            }
         }, cb);
     });
 }
 
 function iteration(conf, done) {
-    const name = capitalizeFirstLetter(conf.name);
+    const name = conf.name;
     const state = {
         startTime: 0,
         progress: 0,
         max: conf.time,
     };
 
+    // TODO: implement before, on, tick, after
     async.waterfall([
         (cb) => {
-            sendEvent(conf, `before${name}`, cb);
+            sendEvent(conf, `before${name}`, () => { cb(); });
+        },
+        (cb) => {
+            sendEvent(conf, name, () => { cb(); });
         },
         (cb) => {
             state.startTime = new Date() / 1000;
@@ -102,12 +180,12 @@ function iteration(conf, done) {
 
 function main() {
     const config = {
-        rest: { name: 'rest', title: 'resting: ', time: 10 },
         work: { name: 'work', title: 'working: ', time: 5 },
+        rest: { name: 'rest', title: 'resting: ', time: 10 },
     };
 
-
     async.forever((next) => {
+        // TODO use the configuration and eachSeries
         async.waterfall([
             (cb) => { iteration(config.work, cb); },
             (cb) => { iteration(config.rest, cb); },
